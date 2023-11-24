997e25f46e7c0ed3230fda37a8ac7834cd16eb04
yilei <yraya123@163.com>
2023-11-24 17:18:07 +0800

    新增创建课程

--- student-plus-content/student-plus-content-api/src/main/java/com/student/content/api/CourseCategoryController.java
+++ student-plus-content/student-plus-content-api/src/main/java/com/student/content/api/CourseCategoryController.java
@@ -1,10 +1,16 @@
 package com.student.content.api;
 
+import com.student.content.model.dto.AddCourseDto;
+import com.student.content.model.dto.CourseBaseInfoDto;
 import com.student.content.model.dto.CourseCategoryTreeDto;
+import com.student.content.service.CourseBaseInfoService;
 import com.student.content.service.CourseCategoryService;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -15,9 +21,20 @@ public class CourseCategoryController {
 
     @Autowired
     private CourseCategoryService courseCategoryService;
+
+    @Autowired
+    private CourseBaseInfoService courseBaseInfoService;
     
     @GetMapping("/course-category/tree-nodes")
     public List<CourseCategoryTreeDto> queryTreeNodes() {
         return courseCategoryService.queryTreeNodes("1");
+    }
+
+    @ApiOperation("新增课程基础信息")
+    @PostMapping("/course")
+    public CourseBaseInfoDto createCourseBase(@RequestBody AddCourseDto addCourseDto){
+        //机构id，由于认证系统没有上线暂时硬编码
+        Long companyId = 1232141425L;
+        return courseBaseInfoService.createCourseBase(companyId,addCourseDto);
     }
 }
--- student-plus-content/student-plus-content-service/src/main/java/com/student/content/service/CourseBaseInfoService.java
+++ student-plus-content/student-plus-content-service/src/main/java/com/student/content/service/CourseBaseInfoService.java
@@ -2,21 +2,26 @@ package com.student.content.service;
 
 import com.student.base.model.PageParams;
 import com.student.base.model.PageResult;
+import com.student.content.model.dto.AddCourseDto;
+import com.student.content.model.dto.CourseBaseInfoDto;
 import com.student.content.model.dto.QueryCourseParamsDto;
 import com.student.content.model.po.CourseBase;
 
 /**
  * 课程基本信息管理业务接口
+ *
  * @author yilei
  */
-public interface CourseBaseInfoService  {
+public interface CourseBaseInfoService {
 
- /**
-  * 课程查询接口
-  * @param pageParams
-  * @param queryCourseParamsDto
-  * @return
-  */
-  PageResult<CourseBase> queryCourseBaseList(PageParams pageParams, QueryCourseParamsDto queryCourseParamsDto);
+    /**
+     * 课程查询接口
+     *
+     * @param pageParams
+     * @param queryCourseParamsDto
+     * @return
+     */
+    PageResult<CourseBase> queryCourseBaseList(PageParams pageParams, QueryCourseParamsDto queryCourseParamsDto);
 
- }
+    CourseBaseInfoDto createCourseBase(Long companyId, AddCourseDto addCourseDto);
+}
