eb100227a26c066de71b188fa6f70747ce26da88
yilei <yraya123@163.com>
2023-12-13 16:29:15 +0800

    整合nacos-1.0

--- student-plus-system/student-plus-system-api/src/main/java/com/student/SystemApplication.java
+++ student-plus-system/student-plus-system-api/src/main/java/com/student/SystemApplication.java
@@ -3,6 +3,7 @@ package com.student;
 import com.spring4all.swagger.EnableSwagger2Doc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
@@ -12,6 +13,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
  *
  * @Description:
  */
+@EnableDiscoveryClient
 @EnableScheduling
 @EnableSwagger2Doc
 @SpringBootApplication
